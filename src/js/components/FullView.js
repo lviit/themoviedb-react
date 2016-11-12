310e573e35d6ab42f127a49ced0e2795257d79e6
lviit <lauri.viitala@wunderkraut.com>
2016-11-12 19:51:44 +0200

    add img load spinner in full view

--- src/js/components/FullView.js
+++ src/js/components/FullView.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
-const FullView = (props) => {
-  let imageBaseUrl = props.config.images.secure_base_url;
-  let fileSize = props.config.images.backdrop_sizes[3];
-  let movie = props.data;
-  return (
-    <div>
-      <img className="movie--image__full" src={imageBaseUrl + fileSize + movie.backdrop_path}></img>
-      <h1>{movie.title}</h1>
-      <h3>{movie.tagline}</h3>
-      <p>{movie.overview}</p>
-    </div>
-  );
+class FullView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      img_loader: true,
+    }
+  }
+
+  render() {
+    let imageBaseUrl = this.props.config.images.secure_base_url;
+    let fileSize = this.props.config.images.backdrop_sizes[3];
+    let movie = this.props.data;
+
+    return (
+      <div>
+        <div className="image-load-container">
+          {this.state.img_loader && <div className="loader"></div>}
+          <img onLoad={ e => this.setState({ img_loader: false })}
+            onError={ e => this.setState({ img_loader: false })}
+            className="movie--image__full"
+            src={imageBaseUrl + fileSize + movie.backdrop_path}
+          />
+        </div>
+        <h1>{movie.title}</h1>
+        <h3>{movie.tagline}</h3>
+        <p>{movie.overview}</p>
+      </div>
+    );
+  }
 };
 
 export default FullView;
