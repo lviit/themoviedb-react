751e4f19ae401f27307cf0bae9ee823f8eb60712
lviit <lauri.viitala@wunderkraut.com>
2017-03-29 18:05:04 +0300

    fix linting issues

--- src/js/services/ApiConnect.js
+++ src/js/services/ApiConnect.js
@@ -1,53 +1,46 @@
+/* eslint no-console: 0 */
+
 import axios from 'axios';
 import settings from './Settings';
 
 export default {
-  getConfig: () => {
-    return axios
-      .get(settings.configUrl)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  getMovies: () => {
-    return axios
-      .get(settings.moviesUrl)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  getGenres: () => {
-    return axios
-      .get(settings.baseUrl + 'genre/movie/list?api_key=' + settings.apiKey)
-      .then(res => res.data.genres)
-      .catch(error => console.log(error));
-  },
-  getMovieFullview: (id) => {
-    return axios
-      .get(settings.baseUrl + 'movie/' + id + '?api_key='  + settings.apiKey)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  getReviews: (id) => {
-    return axios
-      .get(settings.baseUrl + 'movie/' + id + '/reviews?api_key=' + settings.apiKey)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  getCredits: (id) => {
-    return axios
-      .get(settings.baseUrl + 'movie/' + id + '/credits?api_key=' + settings.apiKey)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  getSimilar: (id) => {
-    return axios
-      .get(settings.baseUrl + 'movie/' + id + '/similar?api_key=' + settings.apiKey)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  },
-  Search: (query) => {
-    return axios
-      .get(settings.baseUrl + 'search/movie?api_key=' + settings.apiKey + '&query=' + query)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  }
-}
+  getConfig: () => axios
+    .get(settings.configUrl)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  getMovies: () => axios
+    .get(settings.moviesUrl)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  getGenres: () => axios
+    .get(`${settings.baseUrl}genre/movie/list?api_key=${settings.apiKey}`)
+    .then(res => res.data.genres)
+    .catch(error => console.log(error)),
+
+  getMovieFullview: id => axios
+    .get(`${settings.baseUrl}movie/${id}?api_key=${settings.apiKey}`)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  getReviews: id => axios
+    .get(`${settings.baseUrl}movie/${id}/reviews?api_key=${settings.apiKey}`)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  getCredits: id => axios
+    .get(`${settings.baseUrl}movie/${id}/credits?api_key=${settings.apiKey}`)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  getSimilar: id => axios
+    .get(`${settings.baseUrl}movie/${id}/similar?api_key=${settings.apiKey}`)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+
+  Search: query => axios
+    .get(`${settings.baseUrl}search/movie?api_key=${settings.apiKey}&query=${query}`)
+    .then(res => res.data)
+    .catch(error => console.log(error)),
+};
