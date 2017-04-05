ff371729d91344f25ae42704319b6981974047a8
lviit <lauri.viitala@wunderkraut.com>
2017-04-05 19:18:42 +0300

    useuglifyplugin and defineplugin to reduce size

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,17 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
     new compressionPlugin({
       asset: "[path].gz[query]",
       algorithm: "gzip",
