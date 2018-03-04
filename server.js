a736df4b7d588cb4128d22d0167817717d1ee277
lviit <lauriviitala3000@gmail.com>
2018-03-04 20:55:34 +0200

    config express

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 
 let express = require('express');
 let app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+app.get('*', function(req, res){
+  res.sendfile(__dirname + '/public/index.html');
+});
+
 app.listen(PORT, function () {
   console.log(`Listening on ${ PORT }`);
 });
