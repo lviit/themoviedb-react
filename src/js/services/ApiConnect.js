859fef4a3a744c1e8ad42d0f7bce09506de2bb41
lviit <lauriviitala3000@gmail.com>
2018-03-05 17:48:48 +0200

    api_key from env variable

--- src/js/services/ApiConnect.js
+++ src/js/services/ApiConnect.js
@@ -1,51 +1,63 @@
 /* eslint no-console: 0 */
 
-import axios from 'axios';
-import settings from './Settings';
+import axios from "axios";
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const configUrl = `${baseUrl}configuration?api_key=${process.env.API_KEY}`;
+const moviesUrl = `${baseUrl}discover/movie?primary_release_year=2017&sort_by=popularity.desc&api_key=${process.env.API_KEY}`;
 
 export default {
-  getConfig: () => axios
-    .get(settings.configUrl)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getConfig: () =>
+    axios
+      .get(configUrl)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  getLatestMovies: () => axios
-    .get(settings.moviesUrl)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getLatestMovies: () =>
+    axios
+      .get(moviesUrl)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  getGenres: () => axios
-    .get(`${settings.baseUrl}genre/movie/list?api_key=${settings.apiKey}`)
-    .then(res => res.data.genres)
-    .catch(error => console.log(error)),
+  getGenres: () =>
+    axios
+      .get(`${baseUrl}genre/movie/list?api_key=${process.env.API_KEY}`)
+      .then(res => res.data.genres)
+      .catch(error => console.log(error)),
 
-  getMovieDetails: id => axios
-    .get(`${settings.baseUrl}movie/${id}?api_key=${settings.apiKey}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getMovieDetails: id =>
+    axios
+      .get(`${baseUrl}movie/${id}?api_key=${process.env.API_KEY}`)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  getReviews: id => axios
-    .get(`${settings.baseUrl}movie/${id}/reviews?api_key=${settings.apiKey}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getReviews: id =>
+    axios
+      .get(`${baseUrl}movie/${id}/reviews?api_key=${process.env.API_KEY}`)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  getCredits: id => axios
-    .get(`${settings.baseUrl}movie/${id}/credits?api_key=${settings.apiKey}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getCredits: id =>
+    axios
+      .get(`${baseUrl}movie/${id}/credits?api_key=${process.env.API_KEY}`)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  getSimilar: id => axios
-    .get(`${settings.baseUrl}movie/${id}/similar?api_key=${settings.apiKey}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  getSimilar: id =>
+    axios
+      .get(`${baseUrl}movie/${id}/similar?api_key=${process.env.API_KEY}`)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  Search: query => axios
-    .get(`${settings.baseUrl}search/movie?api_key=${settings.apiKey}&query=${query}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  Search: query =>
+    axios
+      .get(`${baseUrl}search/movie?api_key=${process.env.API_KEY}&query=${query}`)
+      .then(res => res.data)
+      .catch(error => console.log(error)),
 
-  SearchByGenre: genre => axios
-    .get(`${settings.baseUrl}discover/movie?api_key=${settings.apiKey}&with_genres=${genre}`)
-    .then(res => res.data)
-    .catch(error => console.log(error)),
+  SearchByGenre: genre =>
+    axios
+      .get(`${baseUrl}discover/movie?api_key=${process.env.API_KEY}&with_genres=${genre}`)
+      .then(res => res.data)
+      .catch(error => console.log(error))
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
-'use strict';
-let webpack = require('webpack');
-let compressionPlugin = require('compression-webpack-plugin');
-let path = require('path');
-let precss = require('precss');
-let postcssmixins = require('postcss-mixins');
+"use strict";
+let webpack = require("webpack");
+const Dotenv = require('dotenv-webpack');
+let compressionPlugin = require("compression-webpack-plugin");
+let path = require("path");
+let precss = require("precss");
+let postcssmixins = require("postcss-mixins");
 let postcsseach = require("postcss-each");
 let cssnext = require("postcss-cssnext");
 
-let BUILD_DIR = path.resolve(__dirname, 'public');
-let APP_DIR = path.resolve(__dirname, 'src/js');
+let BUILD_DIR = path.resolve(__dirname, "public");
+let APP_DIR = path.resolve(__dirname, "src/js");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: {
-    app: ['babel-polyfill', APP_DIR + '/Main.jsx'],
+    app: ["babel-polyfill", APP_DIR + "/Main.jsx"],
     vendor: [
-      'react',
-      'react-dom',
-      'react-router',
-      'react-addons-css-transition-group',
-      'classnames',
-      'axios'
+      "react",
+      "react-dom",
+      "react-router",
+      "react-addons-css-transition-group",
+      "classnames",
+      "axios"
     ]
   },
   output: {
     path: BUILD_DIR,
-    filename: 'bundle.js',
+    filename: "bundle.js"
     //publicPath: 'public'
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new Dotenv(),
+    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
     /*
     new webpack.optimize.UglifyJsPlugin({
       compress: {
@@ -51,46 +48,46 @@ module.exports = {
     }) */
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
   module: {
     loaders: [
       {
         test: /.jsx?$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         include: APP_DIR,
         query: {
-          presets: ['es2015', 'react', 'stage-3']
+          presets: ["es2015", "react", "stage-3"]
         }
       },
       {
-        test:   /\.pcss$/,
+        test: /\.pcss$/,
         loader: "style-loader"
       },
       {
-        test:   /\.pcss$/,
+        test: /\.pcss$/,
         loader: "css-loader",
         query: {
           modules: true,
-          localIdentName: '[name]__[local]___[hash:base64:5]'
+          localIdentName: "[name]__[local]___[hash:base64:5]"
         }
       },
       {
-        test:   /\.pcss$/,
+        test: /\.pcss$/,
         loader: "postcss-loader"
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=fonts/[name].[ext]'
+        loader: "file-loader?name=fonts/[name].[ext]"
       },
       {
         test: /\.(jpg|png)$/,
-        loader: 'url?limit=25000'
+        loader: "url?limit=25000"
       }
     ]
   },
-  postcss: function () {
-      return [precss, cssnext];
+  postcss: function() {
+    return [precss, cssnext];
   },
   devServer: {
     inline: true,
@@ -99,6 +96,6 @@ module.exports = {
       index: '/public/index.html'
     }, */
     //compress: true,
-    contentBase: './public'
+    contentBase: "./public"
   }
 };
