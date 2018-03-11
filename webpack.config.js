e832a3a5825380169373f2cd5aa7373d4cb701fc
lviit <lauriviitala3000@gmail.com>
2018-03-11 15:07:31 +0200

    fix prod and dev build

--- webpack.config.js
+++ webpack.config.js
@@ -5,118 +5,102 @@ const Dotenv = require("dotenv-webpack");
 const compressionPlugin = require("compression-webpack-plugin");
 const path = require("path");
 
-//const { ifProduction, ifNotProduction } = getIfUtils(process.env.NODE_ENV);
-
 let BUILD_DIR = path.resolve(__dirname, "public");
 let APP_DIR = path.resolve(__dirname, "src/js");
 
-module.exports = {
-  devtool: "source-map",
-  entry: {
-    app: ["babel-polyfill", APP_DIR + "/Main.jsx"]
-  },
-  output: {
-    path: BUILD_DIR,
-    filename: "[name].js",
-    chunkFilename: "[name].js"
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          chunks: "all"
-        }
-      }
-    }
-  },
-  plugins: removeEmpty([
-    new Dotenv({ systemvars: true })
-    //new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-    /*
-    ifProduction(
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: JSON.stringify("production")
-        }
-      })
-    ),
-    ifProduction(
+module.exports = (env, argv) => {
+  const { ifProduction, ifNotProduction } = getIfUtils(argv.mode);
 
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        },
-        sourceMap: true
-      })
-    ),
-    ifProduction(
-      new compressionPlugin({
-        asset: "[path].gz[query]",
-        algorithm: "gzip",
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8
-      })
-    ) */
-  ]),
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        loader: "babel-loader",
-        include: APP_DIR,
-        query: {
-          presets: ["es2015", "react", "stage-3"]
-        }
-      },
-      {
-        test: /\.pcss$/,
-        use: ["style-loader"]
-      },
-      {
-        test: /\.pcss$/,
-        use: [
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: "[path][name]__[local]--[hash:base64:5]"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.pcss$/,
-        use: ["postcss-loader"]
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: "file-loader?name=fonts/[name].[ext]"
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 25000
-            }
-          }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    inline: true,
-    historyApiFallback: {
-      index: "index.html"
+  return {
+    devtool: "source-map",
+    entry: {
+      app: ["babel-polyfill", APP_DIR + "/Main.jsx"]
     },
-    //compress: true,
-    contentBase: "./public"
-  }
+    output: {
+      path: BUILD_DIR,
+      filename: "[name].js",
+      chunkFilename: "[name].js"
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "all"
+          }
+        }
+      }
+    },
+    plugins: removeEmpty([
+      new Dotenv({ systemvars: true }),
+      ifProduction(
+        new compressionPlugin({
+          asset: "[path].gz[query]",
+          algorithm: "gzip",
+          test: /\.js$|\.css$|\.html$/,
+          threshold: 10240,
+          minRatio: 0.8
+        })
+      ),
+    ]),
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          loader: "babel-loader",
+          include: APP_DIR,
+          query: {
+            presets: ["es2015", "react", "stage-3"]
+          }
+        },
+        {
+          test: /\.pcss$/,
+          use: ["style-loader"]
+        },
+        {
+          test: [/\.pcss$/],
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                localIdentName: "[path][name]__[local]--[hash:base64:5]"
+              }
+            }
+          ]
+        },
+        {
+          test: /\.pcss$/,
+          use: ["postcss-loader"]
+        },
+        {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          loader: "file-loader?name=fonts/[name].[ext]"
+        },
+        {
+          test: /\.(jpg|png)$/,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                limit: 25000
+              }
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      inline: true,
+      historyApiFallback: {
+        index: "index.html"
+      },
+      //compress: true,
+      contentBase: "./public"
+    }
+  };
 };
