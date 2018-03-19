44519d84188f8000cbc671c9ac098fe5387a3201
lviit <lauriviitala3000@gmail.com>
2018-03-19 22:45:51 +0200

    enzyme setup

--- src/js/components/Footer/Footer.test.jsx
+++ src/js/components/Footer/Footer.test.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 import Footer from "./Footer";
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 it("renders correctly", () => {
   const tree = shallow(<Footer />);
--- src/js/components/Header/Header.test.jsx
+++ src/js/components/Header/Header.test.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { shallow, mount, render } from "enzyme";
+import { shallow } from "enzyme";
 import Header from "./Header";
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 it("renders correctly", () => {
   const tree = shallow(<Header.WrappedComponent />);
