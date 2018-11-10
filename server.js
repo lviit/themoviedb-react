5ccb39e259ce3fc437859e00803a490fefcf2cd4
lviit <lauriviitala3000@gmail.com>
2018-11-11 00:45:34 +0200

    heroku ssl redirect

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(sslRedirect());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('*', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`);
-});
-
-// redirect to https
-app.use('*', (req, res, next) => {
-  if (!req.secure) {
-    const secureUrl = `https://${req.headers.host}${req.url}`;
-    res.writeHead(301, { Location: secureUrl });
-    res.end();
-  }
-  next();
 });
 
 app.listen(PORT, () => {
